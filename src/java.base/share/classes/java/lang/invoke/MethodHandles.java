9244542eda7fb5458f724db5ed90dc955c97801e
Maurizio Cimadamore <mcimadamore@openjdk.org>
2017-09-25 11:47:33 +0100

    Import changes from condy/jdk

--- src/java.base/share/classes/java/lang/invoke/MethodHandles.java
+++ src/java.base/share/classes/java/lang/invoke/MethodHandles.java
@@ -101,8 +101,10 @@ public class MethodHandles {
      * <p>
      * For any given caller class {@code C}, the lookup object returned by this call
      * has equivalent capabilities to any lookup object
-     * supplied by the JVM to the bootstrap method of an
-     * <a href="package-summary.html#indyinsn">invokedynamic instruction</a>
+     * supplied by the JVM to the
+     * <a href="package-summary.html#bsm">bootstrap method</a> of an
+     * <a href="package-summary.html#indyinsn">invokedynamic instruction</a> or
+     * <a href="package-summary.html#condycon">dynamic constant</a>
      * executing in the same caller class {@code C}.
      * @return a lookup object for the caller of this method, with private access
      */
