44febbee060f98fc544a671906bcf9f5329358aa
Vicente Romero <vromero@openjdk.org>
2018-05-12 14:06:37 -0700

    Updating langtools regression tests after changes to the API

--- test/langtools/tools/javac/specialConstantFolding/harness/tests/InstanceTrackableMethodsTest.java
+++ test/langtools/tools/javac/specialConstantFolding/harness/tests/InstanceTrackableMethodsTest.java
@@ -15,7 +15,7 @@ public class InstanceTrackableMethodsTest extends ConstantFoldingTest {
 
     void test1() {
         ClassDesc string1 = ClassDesc.ofDescriptor("Ljava/lang/String;");
-        ClassDesc stringArr = string1.array();
+        ClassDesc stringArr = string1.arrayType();
         ClassDesc string2 = stringArr.componentType();
         ClassDesc string3 = ClassDesc.ofDescriptor(string2.descriptorString());
         check(string1.descriptorString().equals(string2.descriptorString()));
@@ -27,8 +27,8 @@ public class InstanceTrackableMethodsTest extends ConstantFoldingTest {
     @InstructionInfo(bytecodePosition=3, values={"CONSTANT_Class_info", "[Ljava/lang/String;"})
     void test2() {
         ClassDesc stringClass = ClassDesc.ofDescriptor("Ljava/lang/String;");
-        Class<?> stringArrClass = ldc(stringClass.array());
-        ClassDesc stringArrConst = stringClass.array();
+        Class<?> stringArrClass = ldc(stringClass.arrayType());
+        ClassDesc stringArrConst = stringClass.arrayType();
         Class<?> stringArrClass2 = ldc(stringArrConst);
     }
 }
