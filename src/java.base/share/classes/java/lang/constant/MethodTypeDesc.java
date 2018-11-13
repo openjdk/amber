93512812e8d3987d4deab77db40eadb054f4e169
Vicente Romero <vromero@openjdk.org>
2018-11-13 08:27:50 -0500

    Adding some clarifications to the API

--- src/java.base/share/classes/java/lang/constant/MethodTypeDesc.java
+++ src/java.base/share/classes/java/lang/constant/MethodTypeDesc.java
@@ -34,6 +34,10 @@ import java.util.stream.Stream;
  * A <a href="package-summary.html#nominal">nominal descriptor</a> for a
  * {@linkplain MethodType} constant.
  *
+ * <p>Two {@linkplain MethodTypeDesc} objects are considered {@link Object#equals(Object)}
+ * if they have the same arity, their return types are equal, and each pair of corresponding
+ * parameter types are equal.
+ *
  * @apiNote In the future, if the Java language permits, {@linkplain MethodTypeDesc}
  * may become a {@code sealed} interface, which would prohibit subclassing except
  * by explicitly permitted types.  Non-platform classes should not implement
