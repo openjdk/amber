8af22fcc06bfd3923e10ad3ce8d1584ffcf307ad
Jim Laskey <jlaskey@openjdk.org>
2019-05-08 12:16:40 -0300

    Tests for String::stripIndent String::translateEscapes

---
+++ test/jdk/java/lang/String/TranslateEscapes.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @summary This exercises String#translateEscapes patterns and limits.
+ * @run main/othervm -Xmx2g TranslateEscapes
+ */
+
+public class TranslateEscapes {
+    public static void main(String... arg) {
+        test1();
+        test2();
+    }
+
+    /*
+     * Standard escapes.
+     */
+    static void test1() {
+        verifyEscape("b", '\b');
+        verifyEscape("f", '\f');
+        verifyEscape("n", '\n');
+        verifyEscape("r", '\r');
+        verifyEscape("t", '\t');
+        verifyEscape("'", '\'');
+        verifyEscape("\"", '\"');
+        verifyEscape("\\", '\\');
+    }
+
+    /*
+     * Continuation escapes.
+     */
+    static void test2() {
+        verifyContinue("   abc\\\n   def", "   abc   def");
+        verifyContinue("   abc\\n   \\ def", "   abc\ndef");
+        verifyContinue("   abc\\\n   \\ def", "   abcdef");
+    }
+
+    static void verifyEscape(String string, char ch) {
+        String escapes = "\\" + string;
+        if (escapes.translateEscapes().charAt(0) != ch) {
+            System.err.format("\"%s\" not escape \"%s\"'%n", string, escapes);
+            throw new RuntimeException();
+        }
+    }
+
+    static void verifyContinue(String a, String b) {
+        if (!a.translateEscapes().equals(b)) {
+            System.err.format("\"%s\" not equal \"%s\"%n", a, b);
+            throw new RuntimeException();
+        }
+    }
+}
