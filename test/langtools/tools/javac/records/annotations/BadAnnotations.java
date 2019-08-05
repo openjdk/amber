af5633c92a95285d5282e9f80d3800dc57343bcb
Vicente Romero <vromero@openjdk.org>
2019-08-05 12:56:08 -0400

    Definition of the new annotation target RECORD_COMPONENT, new implementation of the record attribute

--- test/langtools/tools/javac/records/annotations/BadAnnotations.java
+++ test/langtools/tools/javac/records/annotations/BadAnnotations.java
@@ -33,24 +33,28 @@ class BadAnnotations {
     @interface Foo6 {}
     record R6(@Foo6 int i) {}
 
-    @Target({ ElementType.METHOD })
+    @Target({ ElementType.RECORD_COMPONENT })
     @interface Foo7 {}
     record R7(@Foo7 int i) {}
 
-    @Target({ ElementType.PARAMETER })
+    @Target({ ElementType.METHOD })
     @interface Foo8 {}
     record R8(@Foo8 int i) {}
 
-    // no target applies to all
+    @Target({ ElementType.PARAMETER })
     @interface Foo9 {}
     record R9(@Foo9 int i) {}
 
-    // type annotations are allowed too
-    @Target({ ElementType.TYPE_USE })
+    // no target applies to all
     @interface Foo10 {}
     record R10(@Foo10 int i) {}
 
-    @Target({ ElementType.TYPE_PARAMETER })
+    // type annotations are allowed too
+    @Target({ ElementType.TYPE_USE })
     @interface Foo11 {}
     record R11(@Foo11 int i) {}
+
+    @Target({ ElementType.TYPE_PARAMETER })
+    @interface Foo12 {}
+    record R12(@Foo12 int i) {}
 }
