2f6642b35208a96b3533c8b08d9ab0abb8557fd5
Angelos Bimpoudis <angelos.bimpoudis@oracle.com>
2024-03-21 16:44:06 +0100

    Clean up println

--- src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Code.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Code.java
@@ -2229,7 +2229,6 @@ public class Code {
 
     public int newLocal(VarSymbol v) {
         int reg = v.adr = newLocal(v.erasure(types));
-        System.out.println(v.name.toString() + "," + reg);
         addLocalVar(v);
         return reg;
     }
